e92247e4903f60cd4186a002f02156d87549de98
HanMJ <915660592@qq.com>
2017-08-10 14:48:53 +0800

    Initial Commit

---
+++ ChatWithEachOther/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatWithEachOther
+//
+//  Created by MJ on 2017/8/10.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
